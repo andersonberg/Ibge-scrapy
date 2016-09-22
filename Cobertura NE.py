766ed4873560e4de644b8fbb845e9ea56f4fefc1
Anderson Berg <andersonberg@gmail.com>
2016-09-22 11:05:47 -0300

    refactoring

--- Cobertura NE.py
+++ Cobertura NE.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def get_equipment(equip):
+def get_equipments(equip):
     raio_x = equip[equip["equipamento"].map(lambda x: "raio x" in x.encode('utf8').lower() and "dentario" not in x.encode('utf8').lower() and "densitometria" not in x.encode('utf8').lower())]
     mamografo = equip[equip["equipamento"].map(lambda x: "mamografo" in x.encode('utf8').lower())]
     ultrassom = equip[equip["equipamento"].map(lambda x: "ultrassom" in x.encode('utf8').lower())]
@@ -89,7 +89,7 @@ if __name__ == '__main__':
 
             # Obtém a quantidade de máquinas por categoria
             equipamentos_por_cidade = equipamentos[equipamentos["cidade"] == city_code]
-            total_sum = get_equipment(equipamentos_por_cidade)
+            total_sum = get_equipments(equipamentos_por_cidade)
 
             info_dict.update(total_sum)
             city_list.append(info_dict)
@@ -106,54 +106,4 @@ if __name__ == '__main__':
     result_final = result_final.append(city_list, ignore_index=True)
 
     # Trocar o nome da planilha para o Estado sendo processado
-    result_final.to_excel("Cobertura AL.xls")
-
-    curr_city = int(populacao[u"Código do município"][10][:-1])
-    curr_city_population = populacao[u"Total da população 2010"][10]
-    curr_city_name = populacao[u"Nome do município"][10]
-    equip = equipamentos[equipamentos["cidade"] == curr_city]
-
-    raio_x = equip[equip["equipamento"].map(lambda x: "raio x" in x.encode('utf8').lower())]
-    ultrassom = equip[equip["equipamento"].map(lambda x: "ultrassom" in x.encode('utf8').lower())]
-    ressonancia = equip[equip["equipamento"].map(lambda x: "ressonancia" in x.encode('utf8').lower())]
-    tomografo = equip[equip["equipamento"].map(lambda x: "tomógrafo" in x.encode('utf8').lower())]
-    pet_ct = equip[equip["equipamento"].map(lambda x: "pet/ct" in x.encode('utf8').lower())]
-
-    total_sum = {}
-    total_sum.update({"XP": raio_x["existentes"].sum()})
-    total_sum.update({"US": ultrassom["existentes"].sum()})
-    total_sum.update({"MR": ressonancia["existentes"].sum()})
-    total_sum.update({"CT": tomografo["existentes"].sum()})
-    total_sum.update({"MI": pet_ct["existentes"].sum()})
-
-    print total_sum
-
-    sum_xp = raio_x[["existentes"]].sum()
-    sum_us = ultrassom[["existentes"]].sum().append(sum_xp)
-    sum_mr = ressonancia[["existentes"]].sum().append(sum_us)
-    sum_ct = tomografo[["existentes"]].sum().append(sum_mr)
-    sum_mi = pet_ct[["existentes"]].sum().append(sum_ct)
-
-    # df_sum = pd.DataFrame(data=sum_mi, columns=["XP", "US", "MR", "CT", "MI"])
-    # df_sum = df_sum.append(sum_us, ignore_index=True)
-    # pd.merge(df_sum, sum_ultrassom)
-    # df_sum = df_sum.append(sum_ultrassom, ignore_index=True)
-    # df_sum
-    # sum_mi
-
-    result3 = pib[pib[u"Município"] == curr_city_name]
-    result3
-
-    city_pib = float(result3[2012])
-    city_pib_per_capita = float(result3[u"Per capita (R$) 2012"])
-    city_assist = int(result2[u"Assistência_Médica"])
-    city_pib, city_assist
-
-    dict_result = {u"Município": curr_city_name,
-                   u"Produto Interno Bruto (R$ 1.000 )": city_pib,
-                   u"PIB per capita (R$)": city_pib_per_capita,
-                   u"Quantidade (Pessoas)": curr_city_population,
-                   "Vidas Assistidas ANS": city_assist}
-    columns = dict_result.keys()
-    result_final = pd.DataFrame(data=dict_result, index=["Município"])
-    pd.pivot_table(result_final, index=[u"Município"])
+    result_final.to_excel("Cobertura AL.xls", index=False)
