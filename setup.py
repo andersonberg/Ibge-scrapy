2af1ead4a3e31adbafcd271e7f14e607ef4c5318
Anderson Berg <andersonberg@gmail.com>
2016-09-22 23:21:44 -0300

    adding setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ibge-scrapy',
+    version='0.1',
+    author='Anderson Berg',
+    author_email='andersonberg@gmail.com',
+    description='IBGE and ANS data analyzer'
+)
