6116304fb9f628acf6b798376423c432076a9136
Anderson Berg <andersonberg@gmail.com>
2016-09-23 09:30:36 -0300

    moving files; updating requirements

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='ibge-scrapy',
-    version='0.1',
-    author='Anderson Berg',
-    author_email='andersonberg@gmail.com',
-    description='IBGE and ANS data analyzer'
-)
