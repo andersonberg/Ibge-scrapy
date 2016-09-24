cb62ed3ed60e692730e7b16139ca6f881fe2de49
Anderson Berg <andersonberg@gmail.com>
2016-09-23 21:44:30 -0300

    adding states to script

--- cobertura/cobertura_nordeste.py
+++ cobertura/cobertura_nordeste.py
@@ -2,7 +2,17 @@
 import pandas as pd
 import numpy as np
 
-from estados import estados
+estados = {
+     'maranhao': (460, 677),
+     'piaui': (678, 902),
+     'ceara': (903, 1087),
+     'rio grande do norte': (1088, 1255),
+     'paraiba': (1256, 1479),
+     'pernambuco': (1480, 1665),
+     'alagoas': (1666, 1768),
+     'sergipe': (1769, 1844),
+     'bahia': (1845, 2262)
+}
 
 
 def get_equipments(equip):
