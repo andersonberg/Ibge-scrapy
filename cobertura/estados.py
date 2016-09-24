9d4e70cb3f5cfd2b0a325a9904e4cc7ec18c2d4f
Anderson Berg <andersonberg@gmail.com>
2016-09-23 21:04:21 -0300

    creating states list

---
+++ cobertura/estados.py
@@ -0,0 +1,11 @@
+estados = {
+     'maranhão': (460, 677),
+     'piauí': (678, 902),
+     'ceará': (903, 1087),
+     'rio grande do norte': (1088, 1255),
+     'paraíba': (1256, 1479),
+     'pernambuco': (1480, 1665),
+     'alagoas': (1666, 1768),
+     'sergipe': (1769, 1844),
+     'bahia': (1845, 2262)
+}
