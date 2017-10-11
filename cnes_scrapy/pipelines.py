b4de857185c3258fcd976d3953f4edee0247bf74
Anderson Berg <andersonberg@gmail.com>
2017-10-11 00:43:06 -0300

    saving data to mongodb; update requirements and gitignore

--- cnes_scrapy/pipelines.py
+++ cnes_scrapy/pipelines.py
@@ -4,8 +4,26 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from scrapy.conf import settings
 
 
-class SiemensScrapyPipeline(object):
+class CnesScrapyPipeline(object):
+    def __init__(self, *args, **kwargs):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
     def process_item(self, item, spider):
+        self.collection.insert_one(dict(item))
         return item
